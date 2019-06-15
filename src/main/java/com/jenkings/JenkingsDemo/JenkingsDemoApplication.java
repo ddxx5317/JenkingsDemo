493b561b87651ddd9fd078d882e05fbb6fbc5408
zhaoxingwen <zhaoxingwen@sm.vvip-u.com>
2019-06-15 10:36:07 +0800

    jenkings test

---
+++ src/main/java/com/jenkings/JenkingsDemo/JenkingsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jenkings.JenkingsDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkingsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkingsDemoApplication.class, args);
+	}
+
+}
